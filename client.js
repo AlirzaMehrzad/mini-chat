a1a396b9c8ebd6d62ecc428ffe869aa3073de22a
alireza <alireza.me@hotmail.com>
2024-05-29 11:37:29 +0330

    implement chating

--- client.js
+++ client.js
@@ -1,6 +1,7 @@
 import {io} from 'socket.io-client'
-import { EVENT_INFO } from './constants.js';
-
+import { EVENT_INFO, EVENT_MESSAGE } from './constants.js';
+import readline from 'readline/promises'
+import {stdin as input, stdout as output} from 'process'
 const socket = io('ws://localhost:8081');
 
 socket.on('connect', ()=>{
@@ -10,3 +11,14 @@ socket.on('connect', ()=>{
 socket.on(EVENT_INFO, (message) => {
 console.log('[Server Info]:', message)
 })
+
+const rl = readline.createInterface({input, output});
+const fullname = await rl.question('Fullname:', )
+
+async function getInput(){
+    const message = await rl.question('message: ')
+    socket.emit(EVENT_MESSAGE, { sender: fullname, message })
+    await getInput()
+}
+
+getInput()
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import { Server } from 'socket.io'
 import { Socket } from 'socket.io-client';
-import { EVENT_INFO } from './constants.js';
+import { EVENT_INFO , EVENT_MESSAGE} from './constants.js';
 
 const io = new Server(8081);
 
@@ -9,5 +9,9 @@ console.log('Server is started...!')
 io.on('connection', (socket) => {
     console.log('A client has been connected')
     socket.emit(EVENT_INFO, 'Welcome to simple chat :)')
+
+    socket.on(EVENT_MESSAGE, ({sender, message}) => {
+        console.log(`[${sender}]: ${message}`)
+    })
 });
 
